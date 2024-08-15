08d04ea1e21ed4d8d9d111185e28d9fd70b88e9d
Vaidehi Bhambure <vaidehi.bhambure@gmail.com>
2024-08-15 15:37:43 +0200

    Commit changes done testes

--- __tests__/integration/integration.test.js
+++ __tests__/integration/integration.test.js
@@ -1,6 +1,26 @@
-const { numberCruncher } = require('../modules/numberCruncher');
+import numberCruncher from '../../src/modules/calculation/numberCruncher';
 
-test('numberCruncher crunches numbers correctly', () => {
-    const result = numberCruncher(100);
-    expect(result).toBe(42); // Assuming the expected result is 42
+//const { numberCruncher } = require('../modules/numberCruncher');
+
+test('should add two numbers', () => {
+    const result = numberCruncher({ numOne: '5', numTwo: '3', operator: 'add' });
+    expect(result).toBe(8);
+});
+
+test('should subtract two numbers', () => {
+    const result = numberCruncher({ numOne: '5', numTwo: '3', operator: 'subtract' });
+    expect(result).toBe(2);
+});
+
+test('should multiply two numbers', () => {
+    const result = numberCruncher({ numOne: '5', numTwo: '3', operator: 'multiply' });
+    expect(result).toBe(15);
+});
+test('should divide two numbers', () => {
+    const result = numberCruncher({ numOne: '6', numTwo: '3', operator: 'divide' });
+    expect(result).toBe(2);
+});
+test('should percentage two numbers', () => {
+    const result = numberCruncher({ numOne: '50', numTwo: '100', operator: 'percentage' });
+    expect(result).toBe(50);
 });
--- __tests__/unit/unit.test.js
+++ __tests__/unit/unit.test.js
@@ -1,4 +1,10 @@
-const { gcd, exp, percentage } = require('../modules/math');
+import percentage from '../../src/modules/calculation/util/percentage';
+import gcd from '../../src/modules/calculation/util/gcd';
+import exp from '../../src/modules/calculation/util/exp';
+
+//const { gcd, exp, percentage } = require('../modules/math');
+
+
 
 test('GCD of 12 and 8 is 4', () => {
     expect(gcd(12, 8)).toBe(4);
@@ -8,6 +14,6 @@ test('exp(2, 3) is 8', () => {
     expect(exp(2, 3)).toBe(8);
 });
 
-test('20% of 100 is 20', () => {
-    expect(percentage(100, 20)).toBe(20);
+test('20 is 20% of 100', () => {
+    expect(percentage(20, 100)).toBe(20);
 });
